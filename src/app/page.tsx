9e2803342828eb370ae09152313ee83e5185e882
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-07-18 23:06:38 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Layout } from "@/components/layout";
+import { Header } from "@/components/header";
+import { Content } from "@/components/content";
+import { Footer } from "@/components/footer";
+import { Logo } from "@/components/logo";
+
+export default function Page() {
+  return (
+    <Layout>
+      <Header />
+      <Content />
+      <Footer />
+    </Layout>
+  );
+}
---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,7 @@
+import { Red_Hat_Display } from "next/font/google";
+
+export const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["500", "900"],
+  variable: "--font-red-hat-display",
+});
